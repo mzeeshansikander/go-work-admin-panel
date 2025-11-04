820ba9c4cb6336aecc82d906a8dacf2c2f892802
Maaz Mustafa <zeeshan@zenkoders.com>
2025-11-04 15:29:14 +0500

    deployment

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import logo from "../../../public/assets/images/gowork_logo.png";
+import bg_logo from "../../../public/assets/images/gowork_cover.png";
 
 export default function RootLayout({
   children,
@@ -8,13 +10,19 @@ export default function RootLayout({
   return (
     <div className="h-screen overflow-hidden">
       <div className="flex h-screen">
-        <div className=" lg:w-[50%] w-full bg-white h-full">
+        <div className="lg:w-[50%] w-full bg-white h-full">
           <main className="h-full">{children}</main>
         </div>
-        <div className="hidden xl:flex w-[50%] h-full items-center justify-center bg-[url(/assets/images/gowork_cover.png)] bg-cover bg-center overflow-hidden">
+
+        <div
+          className="hidden xl:flex w-[50%] h-full items-center justify-center bg-cover bg-center overflow-hidden"
+          style={{
+            backgroundImage: `url(${bg_logo.src})`,
+          }}
+        >
           <div className="relative w-[40vw] max-w-[400px] 2xl:max-w-[500px] aspect-square min-h-[200px]">
             <Image
-              src="/assets/images/gowork_logo.png"
+              src={logo}
               alt="GoWork Logo"
               fill
               className="object-contain"
